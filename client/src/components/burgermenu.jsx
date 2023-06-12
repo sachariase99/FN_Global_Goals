92d507a493ba795fd55c111b0e198b36b9d60b4e
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-06-12 07:45:51 +0200

    Update burgermenu.jsx

--- client/src/components/burgermenu.jsx
+++ client/src/components/burgermenu.jsx
@@ -18,9 +18,9 @@ const BurgerMenu = () => {
   return (
     <div className='text-white flex justify-end relative md:hidden'>
       <button className={`m-3 z-10 ${isOpen ? 'fixed' : ''}`} onClick={toggleMenu}>
-        <span className={`block w-8 h-1 bg-white mb-1 transform transition-all duration-500 ${isOpen ? 'rotate-45 translate-y-2 bg-white' : ''}`}></span>
-        <span className={`block w-6 h-1 bg-white ml-2 transform transition-all duration-500 ${isOpen ? 'opacity-0' : ''}`}></span>
-        <span className={`block w-8 h-1 bg-white mt-1 transform transition-all duration-500 ${isOpen ? '-rotate-45 -translate-y-2 bg-white' : ''}`}></span>
+        <span className={`block w-7 h-1 bg-white mb-1 transform transition-all duration-500 ${isOpen ? 'rotate-45 translate-y-2 bg-white' : ''}`}></span>
+        <span className={`block w-6 h-1 bg-white ml-1 transform transition-all duration-500 ${isOpen ? 'opacity-0' : ''}`}></span>
+        <span className={`block w-7 h-1 bg-white mt-1 transform transition-all duration-500 ${isOpen ? '-rotate-45 -translate-y-2 bg-white' : ''}`}></span>
       </button>
       {isOpen && (
         <div className="fixed bg-background h-[100vh] w-[100vw]">
